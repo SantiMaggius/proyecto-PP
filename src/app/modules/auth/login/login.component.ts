a5e0c9dc1cf9a31fbc75b5ae5e3455bc236a0e91
nombreg <gonzaloherreracipo@gmail.com>
2023-11-10 11:57:58 -0300

    termine de hacer el login

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { FirestoreService } from 'src/app/shared/services/firestore.service';
 import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +11,54 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
   hide = true;
-  constructor (public servicioAuth:AuthService, public sevicioRouter:Router){
-  }
-  login (){
 
+  usuarios: Usuario = {
+    uid: '',
+    nombre: '',
+    rol: '',
+    email: '',
+    contrasena: ''
   }
+
+  constructor(
+    public servicioAuth: AuthService,
+    public firestoreService: FirestoreService,
+    public router: Router
+    ){}
+  
+    // llamamos función para INICIAR SESIÓN
+    async iniciar(){
+      const credenciales = {
+        email: this.usuarios.email,
+        contrasena: this.usuarios.contrasena
+      }; 
+  
+
+  const res = await this.servicioAuth.iniciarSesion(credenciales.email, credenciales.contrasena)
+  // MÉTODO THEN -> CUANDO FUNCIONA CORRECTAMENTE
+  .then(res => {
+    alert("Ha accedido con éxito :)");
+    console.log(credenciales.email);
+
+    this.router.navigate(['/inicio']);
+  })
+
+    // MÉTODO THEN -> ENCAPSULA UN FALLO
+    .catch(error => {
+      alert("Hubo un error al iniciar sesión :( \n"+error);
+
+      console.log(credenciales.email);
+    })
 }
+
+      // llamamos función para CERRAR SESIÓN
+  async salir(){
+    const res = await this.servicioAuth.cerrarSesion()
+    .then(res => {
+      alert("Ha cerrado sesión con éxito.");
+      console.log(res);
+
+      this.router.navigate(['/inicio']);
+    })
+  }
+ }
